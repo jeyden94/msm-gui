6b95125d3393ce9c9d41ae163a95cc244851dfc5
jeyden94 <jeyden94@gmail.com>
2024-11-05 21:27:13 +0000

    mores stuf

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,22 @@
 class ActorsController < ApplicationController
   
+  def update
+    a_id = params.fetch("the_id")
+
+    matching_records = Actor.where({:id => a_id})
+    the_actor = matching_records.at(0)
+
+    the_actor.name = params.fetch("the_name")
+    the_actor.dob = params.fetch("the_dob")
+    the_actor.bio = params.fetch("the_bio")
+    the_actor.image = params.fetch("the_image")
+
+    the_actor.save
+
+    redirect_to("/actors")
+
+  end
+
   def create
     
     a = Actor.new
